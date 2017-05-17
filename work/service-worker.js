2582c669382e8dc9d7db4d381fe58071248f2145
Chris Edmonds <chris.edmonds128@gmail.com>
2017-05-17 17:55:24 -0600

    still not right

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'weatherPWA-step-6-1';
+var cacheNamo = 'weatherPWA-step-6-1';
 var filesToCache = [
   '/',
   '/index.html',
@@ -22,7 +22,7 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheNamo).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
@@ -34,7 +34,7 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
+        if (key !== cacheNamo) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
